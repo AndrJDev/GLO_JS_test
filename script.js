00d6d49351c3a43aeae07572df14e8592ea5dfa9
AndrJdev <andgggm@gmail.com>
2021-12-13 16:25:55 +0300

    lesson11-fix1

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 let btn = document.getElementById('btn');
 let e_btn = document.getElementById('e_btn');
 let input = document.getElementById('text');
+let input_range = document.getElementById('range');
+let range_span = document.getElementById('range-span');
+
 let square = document.getElementById('square');
 let circle = document.getElementById('circle');
-let input_range = document.getElementById('range');
+
+range_span.textContent = "50";
+
 let inputValue;
 
 e_btn.style.display = "none";
@@ -18,10 +23,9 @@ btn.onclick = function () {
     input.value = "";
 };
 
-console.dir(input_range);
-
 input_range.oninput = function () {
     let input_range = document.getElementById('range');
     let circle = document.getElementById('circle');
+    range_span.textContent = input_range.value;
     circle.style.width = input_range.value + '%';
 };
